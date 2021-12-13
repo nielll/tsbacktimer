27e28e5d6003aa157774735c50fa6cb9ccad4b43
Nico Elmer <nico_elmer@niel.site>
2021-12-13 07:44:15 +0100

    webpack libraryexport default

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
    output: {
        filename: "bundle.js",
        path: path.resolve(__dirname, 'public/dist'),
-       library: 'ts', //add this line to enable re-use
+       library: 'tsBackTiming', //add this line to enable re-use
        libraryExport: 'default',  // export the default as window.MyClass
    },
    resolve: {
